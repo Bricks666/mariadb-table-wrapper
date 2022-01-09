c178e216b139993c0718f7bcb83585ca4cc99a5d
Bricks666 <tconckoff82@gmail.com>
2022-01-09 22:35:18 +0300

    some change

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { Table } from "./lib";
 export * from "./types";
 export * from "./utils";
-export * from "mariadb";
--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,24 @@ const path = require("path");
 module.exports = {
 	mode: "production",
 	entry: "./src/index.ts",
+	experiments: {
+		outputModule: true,
+	},
 
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "index.js",
 		clean: true,
-		globalObject: "this",
 		library: {
-			type: "umd2",
+			type: "commonjs-module",
 		},
 	},
 
 	resolve: {
 		extensions: [".ts"],
+	},
+	externals: {
+		mariadb: "mariadb",
 	},
 	module: {
 		rules: [
