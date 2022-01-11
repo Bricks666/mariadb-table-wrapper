72d6db65dd5f5499b522f8b1252719e595b0ed90
Bricks666 <tconckoff82@gmail.com>
2022-01-11 23:10:15 +0300

    fix error in filter parse

--- src/utils/queryParsers/parseWhere.ts
+++ src/utils/queryParsers/parseWhere.ts
@@ -16,7 +16,7 @@ export const parseWhere = <T extends AnyObject>(
 		if (isArray(values[i])) {
 			filter += `IN (${parseSQLValues(values[i])})`;
 		} else {
-			filter += `= '${values[i]}'`;
+			filter += `= '${values[i] || null}'`;
 		}
 
 		return filter;
