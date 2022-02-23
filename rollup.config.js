6a1901cacdb8baf012e4de173826bc72dfd6973c
Brciks666 <tconckoff82@gmail.com>
2022-02-23 15:19:46 +0300

    some changes in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,33 @@
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 
 import pkg from "./package.json";
 
+const generateOutput = (format) => {
+	/** @type {import("rollup").OutputOptions} */
+	return {
+		file: outputPath[format],
+		exports: "named",
+		format,
+	};
+};
+
+const outputPath = {
+	cjs: pkg.main,
+	es: pkg.module,
+};
+
 /** @type {import("rollup").RollupOptions} */
 export default {
 	input: "./src/index.ts",
 	external: [/.json/, /node_modules/],
-	output: [
-		{
-			file: pkg.main,
-			format: "cjs",
-			exports: "named",
-		},
-		{
-			file: pkg.module,
-			format: "es",
-			exports: "named",
-		},
-	],
+	output: [generateOutput("cjs"), generateOutput("es")],
 	plugins: [
 		resolve(),
 		typescript({
-			tsconfig: "./tsconfig.json",
+			tsconfig: "tsconfig.json",
 		}),
 		commonjs(),
 		babel({
